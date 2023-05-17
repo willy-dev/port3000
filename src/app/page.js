5b4c5751e14064078388a205e030de3d3b7600cb
willy-dev <marete015@gmail.com>
2023-05-17 18:23:58 +0300

    solved responsiveness

--- src/app/page.js
+++ src/app/page.js
@@ -30,7 +30,7 @@ export default function Home() {
       <main className="bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900">
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between">
-            <h1 className="text-xl font-burtons dark:text-white">developedbyMareh</h1>
+            <h1 className="text-xl font-burtons sm:text-xs pr-10 md:text-xl dark:text-white">developedbyMareh</h1>
             <ul className="flex items-center">
               <li>
                 <BsFillMoonStarsFill 
